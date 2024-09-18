bb153d40e2c372c4da866c53464af8ebcdb4e522
xDec0de <xdec0deyt@gmail.com>
2024-09-18 20:06:12 +0200

    Update ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 19:32:30 by daniema3          #+#    #+#             */
-/*   Updated: 2024/09/16 20:53:41 by daniema3         ###   ########.fr       */
+/*   Updated: 2024/09/18 18:53:27 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*str;
+	size_t	final_size;
 
-	str = malloc(size * count);
+	final_size = size * count;
+	str = malloc(final_size);
 	if (str == NULL)
 		return (NULL);
-	ft_bzero(str, size * count);
+	ft_bzero(str, final_size);
 	return (str);
 }
