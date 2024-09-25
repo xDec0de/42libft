85059b5cb2af085747deae7738d448c23759f387
xDec0de <xdec0deyt@gmail.com>
2024-09-25 16:27:53 +0200

    Fix & document ft_lstnew

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 19:33:50 by daniema3          #+#    #+#             */
-/*   Updated: 2024/09/16 20:58:59 by daniema3         ###   ########.fr       */
+/*   Updated: 2024/09/25 16:16:03 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	struct s_list	*list;
 
-	list = malloc(sizeof(content));
+	list = malloc(sizeof(content) + sizeof(NULL));
 	if (list == NULL)
 		return (NULL);
 	list->content = content;
